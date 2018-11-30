612bcbaaa6fc8903049e3cd3fc65287a07aec977
Quentin Silva <Sirius242.hd@gmail.com>
2018-11-30 10:12:43 +0100

    rename header to Theheader & edit navbar

--- components/Theheader/Theheader.vue
+++ components/Theheader/Theheader.vue
@@ -2,9 +2,9 @@
     <header class="main-header">
         <nav class="main-nav">
             <ul class="nav-links">
-                <nuxt-link to="/" tag="li">Home</nuxt-link>
-                <nuxt-link to="/popup" tag="li">Popup</nuxt-link>
-                <nuxt-link to="/about" tag="li">About</nuxt-link>
+                <li><nuxt-link to="/"      tag="a" class="nav-links">Home</nuxt-link></li>
+                <li><nuxt-link to="/popup" tag="a" class="nav-links">Popup</nuxt-link></li>
+                <li><nuxt-link to="/about" tag="a" class="nav-links">About</nuxt-link></li>
             </ul>
         </nav>
     </header>
@@ -17,5 +17,37 @@
         left: 0;
         width: 100%;
         background: #022d30;
+        height: 4.5rem;
+    }
+    .nav-links {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+    .main-nav {
+        height: 100%;
+    }
+    .nav-links {
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 0 1rem
+    }
+    .nav-links a {
+        display: block;
+        text-decoration: none;
+        color: white;
+    }
+    .nav-links a:hover,
+    .nav-links a:active {
+        color: #06c4d1
+    }
+    .nav-links.nuxt-link-active a {
+        color: #06c4d1;
     }
 </style>
