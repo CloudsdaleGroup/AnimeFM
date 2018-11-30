329b627f310a1bdba737a93bb8eb2b86f1981fc5
Quentin Silva <Sirius242.hd@gmail.com>
2018-11-30 09:30:46 +0100

    add header

---
+++ components/Theheader/Theheader.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="header">
+        <nav class="nav">
+            <ul class="links">
+                <nuxt-link to="/" tag="li">Home</nuxt-link>
+                <nuxt-link to="/popup" tag="li">Popup</nuxt-link>
+                <nuxt-link to="/about" tag="li">About</nuxt-link>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+    .header {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        background: #022d30;
+    }
+</style>
