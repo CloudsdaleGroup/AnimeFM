b37454b148491c0e876a635c47a26a5979622e4b
Quentin Silva <Sirius242.hd@gmail.com>
2018-12-03 00:25:00 +0100

    add jquerry, add shoutcastJS from temp cdn

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 const pkg = require('./package')
 
+
 module.exports = {
   mode: 'universal',
 
@@ -14,7 +15,8 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     script: [
-      { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' }
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' },
+      { src: 'https://cdn.siriushd.net/shoutcast.js'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
